eb12f8fe4eca9b0d003d65975873d2fed4106adc
Artem Bednyakov <artb@list.ru>
2024-04-09 16:53:25 +0200

    Add Date Base

---
+++ tools/dbmanager.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+def create_database(name: str = 'translation') -> None:
+    with sqlite3.connect(f'{name}.db') as conn:
+        cursor = conn.cursor()
+
+        cursor.execute('''CREATE TABLE IF NOT EXISTS en_ru_dict (
+                        id INTEGER PRIMARY KEY,
+                        words TEXT)''')
+
+
+def insert_data(data: list) -> None:
+    with sqlite3.connect('translation.db') as conn:
+        cursor = conn.cursor()
+
+        for item in data:
+            count, *_ = cursor.execute(f"SELECT COUNT (*) FROM en_ru_dict WHERE words = '{item}'").fetchone()
+            if count == 0:
+                cursor.execute("INSERT INTO en_ru_dict (words) VALUES (?)", (item,))
+
+
+def get_data() -> list:
+    with sqlite3.connect('translation.db') as conn:
+        cursor = conn.cursor()
+
+        rows = cursor.execute("SELECT words FROM en_ru_dict").fetchall()
+        data = [item[0] for item in rows]
+
+        return data
+
+
+def delete_word_from_db(word: str) -> None:
+    with sqlite3.connect('translation.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM en_ru_dict WHERE words = ?", (word,))
+
+
+
+if __name__ == '__main__':
+    raise Exception('Hello, it is modul!')
