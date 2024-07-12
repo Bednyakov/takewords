dea9d9ef872441a2b95a1d3bf1253e6dbb1982dc
Artem Bednyakov <artb@list.ru>
2024-07-12 18:18:13 +0300

    add tests

---
+++ tests/test_app.py
@@ -0,0 +1,44 @@
+import pytest
+from routes import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_index_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'TakeWords' in response.data
+
+def test_translate_route(client):
+    response = client.get('/translate')
+    assert response.status_code == 200
+    assert b'TakeWords' in response.data
+
+def test_nonexistent_route(client):
+    response = client.get('/nonexistent')
+    assert response.status_code == 404
+    assert b'Error 404' in response.data
+
+def test_get_from_date(client):
+    response = client.get('/api/v1.0/requests/yyyy-mm-dd')
+    assert response.status_code == 200
+    assert b'sorry' in response.data
+
+def test_get_from_correct_date(client):
+    response = client.get('/api/v1.0/requests/2024-07-12')
+    assert response.status_code == 200
+    assert b'user' in response.data
+
+def test_get_user_url(client):
+    response = client.get('/api/v1.0/user_requests/username')
+    assert response.status_code == 200
+    assert b'sorry' in response.data
+
+def test_translate_text(client):
+    response = client.get('/api/v1.0/translate?text=hello')
+    assert response.status_code == 200
+    assert b'u' in response.data
+
+
